d077f3b75dfd0e39c3209568b965fc266ae7f22b
shamim-simplilearn <shamim.gcloud@gmail.com>
2021-05-29 16:05:05 +0530

    Initial commit

---
+++ snake.py
@@ -0,0 +1,54 @@
+
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        segment = Turtle("square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(position)
+        self.segments.append(segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for seg_index in range((len(self.segments) - 1), 0, -1):       # from 2 to 0 index with step size of 1 backwards
+            prev_x = self.segments[seg_index - 1].xcor()
+            prev_y = self.segments[seg_index - 1].ycor()
+            self.segments[seg_index].goto(prev_x, prev_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
