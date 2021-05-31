fe17b9c99459b44a65e9b9bc06d820dca9eee69a
shamim-simplilearn <shamim.gcloud@gmail.com>
2021-05-31 23:39:41 +0530

    high score data file

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,9 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        # self.high_score = 0
+        with open("data.txt", mode='r') as data:
+            self.high_score = int(data.read())
         self.color('white')
         self.penup()
         self.goto(0, 270)
@@ -27,6 +29,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode='w') as data:
+                data.write("{}".format(self.high_score))
         self.score = 0
         self.update_score()
 
